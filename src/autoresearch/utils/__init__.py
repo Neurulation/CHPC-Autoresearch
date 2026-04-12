f1ff108f571cf9e6ecd5e56c8f51e93a4d9fe517
Arné Schreuder <arneschreuder@icloud.com>
2026-04-12 10:43:49 +0200

    utils: export rate_encode and ttfs_encode from spike_encoding

--- src/autoresearch/utils/__init__.py
+++ src/autoresearch/utils/__init__.py
@@ -4,6 +4,7 @@ from .data import create_dataloaders
 from .device import setup_device
 from .evaluation import validate
 from .reproducibility import set_seed
+from .spike_encoding import rate_encode, ttfs_encode
 from .wandb_utils import initialize_wandb
 
 __all__ = [
@@ -11,5 +12,7 @@ __all__ = [
     "setup_device",
     "validate",
     "set_seed",
+    "rate_encode",
+    "ttfs_encode",
     "initialize_wandb",
 ]
